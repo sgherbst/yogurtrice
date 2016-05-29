49dd60ea605fa7b66c61f6df30db9da312da5121
Steven Herbst <sgherbst@Stevens-MacBook-Pro.local>
2016-05-28 21:13:33 -0700

    integrated auto-dispensing function and merged in old state-machine code for controlling temperature.

--- yogurtrice.ino
+++ yogurtrice.ino
@@ -1,32 +1,108 @@
 #include <OneWire.h>
 
+// IO pin definitions
 #define ONE_WIRE_PIN      10
 #define RELAY_PIN         13
+#define MOTOR_ENABLE       9
+#define MOTOR_INPUT        8
+
+// Serial settings
 #define BAUD_RATE         9600
-#define INOCULATE_TEMP_F  110.0
+
+// System states
+#define SCALD       0
+#define COOL        1
+#define DISPENSE    2
+#define INOCULATE   3
+#define DONE        4
+
+// Yogurt parameters
+#define SCALD_TEMP_F          180.0
+#define DISPENSE_TIME_MIN     2.0
+#define INOCULATE_TEMP_F      110.0
+#define INOCULATE_HYST_F      0.5
+#define INOCULATE_TIME_HR     12.0 
+
+// Temperature sensor parameters
 #define ERROR_TEMP_C      10000.0  // defined large enough to ensure the heater will be disabled in case of an error
 
 OneWire  ds(ONE_WIRE_PIN);
 
+// Persistent variables
+int State;
+unsigned long inoculateStart;
+unsigned long dispenseStart;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
+  
+  // Initialize pin modes
   pinMode(RELAY_PIN, OUTPUT);
+  pinMode(MOTOR_ENABLE, OUTPUT);
+  pinMode(MOTOR_INPUT, OUTPUT);
+
+  // Initialize serial port
   Serial.begin(BAUD_RATE);
+
+  // Initialize yogurt state machine
+  State=SCALD;
+
+  // Initialize pin states
+  enable_heater();
+  motor_off();
 }
 
 void loop(void) {
+  float inoculateDurationHr;
+  float dispenseDurationMin;
   float temp_f;
-  
+
   temp_f = get_temp_f();
   Serial.print("Temperature: ");
   Serial.print(temp_f);
-
-  if (temp_f >= INOCULATE_TEMP_F){
-    disable_heater();
-    Serial.println(", heater disabled.");
+  Serial.print(" (degF), State=");
+  
+  if (State==SCALD){
+    Serial.println("SCALD");
+    if (temp_f >= SCALD_TEMP_F){
+      disable_heater();
+      State=COOL;
+    }
+  } else if (State==COOL) {
+    Serial.println("COOL");
+    if (temp_f <= INOCULATE_TEMP_F){
+      dispenseStart = millis();
+      motor_on(); 
+      State=DISPENSE;
+    }
+  } else if (State==DISPENSE) {
+    dispenseDurationMin = ((float)(millis()-dispenseStart))/60000.0;
+    Serial.print("DISPENSE, Duration=");
+    Serial.print(dispenseDurationMin);
+    Serial.println(" (min)");
+    if (dispenseDurationMin >= DISPENSE_TIME_MIN) {
+      motor_off();
+      inoculateStart = millis();
+      State=INOCULATE;
+    }
+  } else if (State==INOCULATE) {
+    inoculateDurationHr = ((float)(millis()-inoculateStart))/3600000.0;
+    Serial.print("INOCULATE, Duration=");
+    Serial.print(inoculateDurationHr);
+    Serial.println(" (hr)");
+    if (inoculateDurationHr < INOCULATE_TIME_HR){
+      if (temp_f > (INOCULATE_TEMP_F+INOCULATE_HYST_F)){
+        disable_heater();
+      } else if (temp_f < (INOCULATE_TEMP_F-INOCULATE_HYST_F)){
+        enable_heater();
+      }
+    } else {
+      disable_heater();
+      State=DONE;      
+    }
   } else {
-    enable_heater();
-    Serial.println(", heater enabled.");
+    Serial.println("DONE");
+    State=DONE;
   }
 }
 
@@ -36,6 +112,16 @@ void enable_heater(){
 
 void disable_heater(){
   digitalWrite(RELAY_PIN, LOW);
+}
+
+void motor_on(){
+  digitalWrite(MOTOR_ENABLE, HIGH);
+  digitalWrite(MOTOR_INPUT, HIGH);
+}
+
+void motor_off(){
+  digitalWrite(MOTOR_ENABLE, LOW);
+  digitalWrite(MOTOR_INPUT, LOW);
 }
 
 //
